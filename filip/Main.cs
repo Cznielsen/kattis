c80c5e6a1892d8377cc84829e33beeb88f78e0a7
Christian Zhuang-Qing Nielsen <czqnielsen@live.dk>
2019-09-19 10:44:33 +0200

    finished filip and janitortroubles

---
+++ filip/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace filip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            char[] arr0 = input[0].ToCharArray();
+            char[] arr1 = input[1].ToCharArray();
+            Array.Reverse(arr0);
+            Array.Reverse(arr1);
+
+            int num0 = Int32.Parse(new string(arr0));
+            int num1 = Int32.Parse(new string(arr1));
+
+            Console.WriteLine(num0 > num1 ? num0 : num1);
+
+        }
+    }
+}
---
+++ janitortroubles/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace janitortroubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            double[] values = new double[4];
+            for (int i = 0; i < 4; i++)
+            {
+                values[i] = Double.Parse(input[i]);
+            }
+            double sp = (values[0] + values[1] + values[2] + values[3])/2d;
+            double result = Math.Sqrt((sp-values[0]) * (sp-values[1]) * (sp - values[2]) * (sp-values[3]));
+            Console.WriteLine(result);
+        }
+    }
+}
